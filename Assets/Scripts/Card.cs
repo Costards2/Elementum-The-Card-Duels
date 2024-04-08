8e3aae38513c752c69cd8ea6650da46777857c42
Pedro Costard Oliveira <100092162+Costards2@users.noreply.github.com>
2024-04-08 18:13:58 -0300

    Layout II

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    public enum Type { Fire, Water, Plant }
+    public Type selectedType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
